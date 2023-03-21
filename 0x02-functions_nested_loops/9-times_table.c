63b7cfb7b5ce6c5dafda90ee4072774f53b80a85
aganokelly <kmarkagano@gmail.com>
2023-03-21 14:42:49 -0700

    tbl

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - prints tables
+ * times_table - prints 9 times table
  * Return: empty output
  */
 
@@ -11,29 +11,29 @@ void times_table(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (j = 0 ; j <= 9 ; j++)
-		{
-			k = j * i;
-			if (k > 9)
-			{
-				u = k % 10;
-				d = (k - u) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
-			}
-			else
-			{
-			if (j != 0)
-			{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-			}
-				_putchar(k + '0');
-
-			}
-		}
-	}	_putchar('\n');
+	for (j = 0 ; j <= 9 ; j++)
+	{
+	k = j * i;
+	if (k > 9)
+	{
+	u = k % 10;
+	d = (k - u) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(d + '0');
+	_putchar(u + '0');
+	}
+	else
+	{
+	if (j != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(k + '0');
+	}
+	}
+	_putchar('\n');
+	}
 }
