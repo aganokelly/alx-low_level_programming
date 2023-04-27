ec2af87aad93931bddcd038846bc9c4f7b75c287
aganokelly <kmarkagano@gmail.com>
2023-04-27 08:43:30 -0700

    fii

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t k = 0;
+	size_t z = 0;
 
 
 	while (h != NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		k++;
+		z++;
 	}
-	return (k);
+	return (z);
 }
