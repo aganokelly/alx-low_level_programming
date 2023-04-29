29c65bfe5199404e2af25499f1f8b8b1e0e22224
aganokelly <kmarkagano@gmail.com>
2023-04-29 10:31:16 -0700

    dee

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *md = *head;
 	unsigned int kel;
 
-	if (*head == NULL)
+	if (md == NULL)
 		return (-1);
 	if (index == 0)
 	{
