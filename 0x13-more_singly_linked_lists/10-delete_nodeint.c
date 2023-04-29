10124d92c0a2d04c63330ddeb743008461e56460
aganokelly <kmarkagano@gmail.com>
2023-04-29 10:18:32 -0700

    delet

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -6,8 +6,7 @@
  * @head: A pointer  of the listint_t list.
  * @index: The index of the node to be deleted - indices start at 0.
  *
- * Return: On success - 1.
- *         On failure - -1.
+ * Return: On success - 1. On failure - -1.
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
@@ -16,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *md = *head;
 	unsigned int kel;
 
-	if (md == NULL)
+	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
