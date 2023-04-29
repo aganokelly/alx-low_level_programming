243599faca56f16b9b15749284f413feb59ddb79
aganokelly <kmarkagano@gmail.com>
2023-04-29 10:46:45 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t mod = 0;
 
 	while (h)
 	{
+		mod++;
 		printf("%d\n", h->n);
-		nodes++;
 		h = h->next;
 	}
-	return (nodes);
+
+	return (mod);
 }
